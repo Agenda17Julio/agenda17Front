1d6eaacbe576c3f45cacecaebb3cdcaef9c1216c
Jhoel Malte <jowell29jm@gmail.com>
2021-05-11 14:47:52 -0500

    Vista Login terminada

--- src/app/components/pages/auth/signin.tsx
+++ src/app/components/pages/auth/signin.tsx
@@ -1,13 +1,37 @@
-const SigninScreen = () => {
-    return <section className='signin_main'>
+import {Link} from 'react-router-dom';
 
-        <div className="signin_section_1">
-            <h1 className='signin_section_1_title'>SigninScreen</h1>
+const SigninScreen = () => {
+    return <section className='signin_section_main_contenedor'>
+
+        <div className="signin_section_container">
+            <div className="signin_section_container_forms-container">
+                <div className="signin_section_container_forms-container_signin-signup">
+                    <form className="section_signin_form">
+                        <h2 className="section_signin_form_title">Logearse</h2>
+                        <div className="section_signin_form_input-field">
+                            <span className="material-icons">
+                                person</span>
+                            <input type="text" name="email" placeholder="Username" />
+                        </div>
+                        <div className="section_signin_form_input-field">
+                            <span className="material-icons">
+                                vpn_key</span>
+                            <input type="password" name="password" placeholder="Password" />
+                        </div>
+                        <input type="submit" value="Iniciar Sesion" className="section_signin_form btn solid" />
+                       
+                       
+                    </form>
+                </div>
+            </div>
         </div>
 
         <div className="signin_section_2">
-            
+
+
         </div>
+
+
 
     </section>
 }
